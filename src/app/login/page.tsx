8417039b1b34cb306dc6bcefc524037797fa6505
ahmadeq <114425070+ahmadeq@users.noreply.github.com>
2025-06-05 19:32:41 +0300

    Update page.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,4 +1,5 @@
 import SignInForm from "@/components/login/signin-form";
+// TODO remove this comment
 export default function Home() {
   return (
     <div className="flex flex-col min-h-screen items-center justify-center  ">
